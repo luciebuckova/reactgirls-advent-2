c9714a475f79d58c8cd83d8d1f6541a8e704a5a8
Lucie Bučková <lucie.buckova@gmail.com>
2023-12-17 17:09:30 +0100

    Add styling and basic functions

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const API_URL =
-  'https://api.themoviedb.org/3/discover/movie?&page=1&sort_by=popularity.desc&api_key=6c3cf8b7266b7efd0fb0cc854e1578bc';
+  'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=6c3cf8b7266b7efd0fb0cc854e1578bc';
 
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=6c3cf8b7266b7efd0fb0cc854e1578bc&query="';
 
-const main = document.getElementById('main');
+const main = document.querySelector('.main__content');
 const search = document.getElementById('search');
-
-// Get initial movies
 
 getMovies(API_URL);
 
@@ -27,11 +25,11 @@ function showMovies(movies) {
     const { title, poster_path, id } = movie;
 
     const movieEl = document.createElement('div');
-    movieEl.classList.add('movie');
+    movieEl.classList.add('movie__card');
 
     movieEl.innerHTML = `<img onClick="getMovie(${id})" src="${
       IMG_PATH + poster_path
-    }" alt="${title}">`;
+    }" alt="${title}" class="movie__image">`;
     main.appendChild(movieEl);
   });
 }
@@ -43,35 +41,44 @@ async function getMovie(id) {
     const response = await fetch(movieDetailsURL);
     const movieDetails = await response.json();
 
-    // Extract relevant information from movieDetails and display it on the page
     const { title, overview, release_date, vote_average, poster_path } =
       movieDetails;
 
+    const releaseYear = new Date(release_date).getFullYear();
+    const roundedVoteAverage = vote_average.toFixed(1);
+
     main.innerHTML = `
-      <div class="movie-info">
-      <div>
-      <img onClick="getMovie(${id})" src="${
+      <div class="movie-overview">
+        <div class="movie-overview__image">
+          <img onClick="getMovie(${id})" src="${
       IMG_PATH + poster_path
     }" alt="${title}">
-    </div>
-    <div>
-      <h2>${title}</h2>
-      <p>${overview}</p>
-      <p>Release Date: ${release_date}</p>
-      <p>Vote Average: ${vote_average}</p>
-      <button onClick="goToHome()">Back to Home</button>
-      </div>
+    <div class="vote-flag">${roundedVoteAverage}</div>
+        </div>
+        <div class="movie-overview__text">
+          <div>
+            <p class="overview">${overview}</p>
+            <p><span class="bold-text">Release Date:</span> ${releaseYear}</p>
+          </div>
+          <div><button class="movie-overview__btn" onClick="goToHome()">Back to Home</button></div>
+        </div>
       </div>  
     `;
+
+    const mainHeading = document.querySelector('.main__heading');
+    if (mainHeading) {
+      mainHeading.innerText = title;
+    }
+
     main.classList.add('one-movie');
+    search.style.display = 'none';
   } catch (error) {
     console.error('Error fetching movie details:', error);
-    // Handle the error or provide feedback to the user
   }
 }
 
 function goToHome() {
-  window.location.reload(); // You can adjust this based on your actual home screen logic
+  window.location.reload();
 }
 
 search.addEventListener('input', () => {
